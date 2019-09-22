f973fcaf712fb7d2fb8a01b2d959bc89511460d6
Kyon Lee <kyon.h.lee@gmail.com>
2019-09-22 15:47:26 +0900

    Add express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('hello world')
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+	res.send(`
+        <div>
+            <h1>Hi there!</h1>
+        </div>
+    `);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}`);
+});
