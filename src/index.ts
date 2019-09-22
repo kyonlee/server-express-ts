f1d275bbe0d2f77c2a3211835cbba51e6a45c339
Kyon Lee <kyon.h.lee@gmail.com>
2019-09-22 17:33:56 +0900

    Add loginRoutes

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import bodyPaser from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { router } from './routes/loginRoutes';
 
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-	res.send(`
-        <div>
-            <h1>Hi there!</h1>
-        </div>
-    `);
-});
+app.use(bodyPaser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['hfiuewhqf'] }));
+app.use(router);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
