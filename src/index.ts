131160d1a23445d3d674f32541ad63cbc4833d96
Kyon Lee <kyon.h.lee@gmail.com>
2019-09-28 16:57:25 +0900

    Refactor remaining routes into controllers

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,14 @@ import express from 'express';
 import bodyPaser from 'body-parser';
 import cookieSession from 'cookie-session';
 
-import './routes/loginRoutes';
-import './controllers/LoginController';
 import { AppRouter } from './AppRouter';
+import './controllers/RootController';
+import './controllers/LoginController';
 
 const app = express();
 
 app.use(bodyPaser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['hfiuewhqf'] }));
-// app.use(router);
 app.use(AppRouter.getInstance());
 
 const PORT = process.env.PORT || 3000;
